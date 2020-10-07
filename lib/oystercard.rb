c1f53be1ec6830d87106689ab6f21016b80865cf
Otis <otisvickersgraver@gmail.com>
2020-10-07 14:15:20 +0100

    initial commit

---
+++ lib/oystercard.rb
@@ -0,0 +1,14 @@
+class Oystercard
+  attr_reader :balance
+
+  BALANCE_LIMIT = 90
+
+  def initialize
+    @balance = 0
+  end
+
+  def top_up(added_balance)
+    raise "Top-up failed. Balance limit of #{BALANCE_LIMIT} exceeded." if @balance + added_balance > BALANCE_LIMIT
+    @balance += added_balance
+  end
+end
---
+++ spec/oystercard_spec.rb
@@ -0,0 +1,18 @@
+require 'oystercard'
+
+describe Oystercard do
+  it 'gives a default balance of 0' do
+    expect(subject.instance_variable_get(:@balance)).to eq 0
+  end
+
+  it 'adds to the balance' do
+    subject.top_up(10)
+    expect(subject.instance_variable_get(:@balance)).to eq 10
+  end
+
+  it 'raises an error if balance limit is exceeded' do
+    balance_limit = Oystercard::BALANCE_LIMIT
+    subject.top_up(balance_limit)
+    expect{ subject.top_up(1) }.to raise_error(RuntimeError, "Top-up failed. Balance limit of #{balance_limit} exceeded.")
+  end
+end
