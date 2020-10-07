4e944338c9dc1dbb05cadf2695b389ef5d538503
Otis <otisvickersgraver@gmail.com>
2020-10-07 15:11:42 +0100

    C10 charging for the journey

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,6 +2,8 @@ class Oystercard
   attr_reader :balance
 
   BALANCE_LIMIT = 90
+  MINIMUM_PERMITTED_FARE = 1
+
 
   def initialize
     @balance = 0
@@ -13,20 +15,24 @@ class Oystercard
     @balance += added_balance
   end
 
-  def deduct(deducted_balance)
-    @balance -= deducted_balance
-  end
-
   def touch_in
+    raise 'Not enough money on card' if @balance < MINIMUM_PERMITTED_FARE
     @in_journey = true
   end
 
   def touch_out
+    deduct
     @in_journey = false
   end
 
   def in_journey?
     return @in_journey
   end
-  
+
+  private
+
+  def deduct(deducted_balance = MINIMUM_PERMITTED_FARE)
+    @balance -= deducted_balance
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,41 +1,56 @@
 require 'oystercard'
 
 describe Oystercard do
-  it 'gives a default balance of 0' do
-    expect(subject.instance_variable_get(:@balance)).to eq 0
+  describe "balance" do
+    it 'gives a default balance of 0' do
+      expect(subject.balance).to eq 0
+    end
   end
 
   it 'adds to the balance' do
     subject.top_up(10)
-    expect(subject.instance_variable_get(:@balance)).to eq 10
-  end
-
-  it 'raises an error if balance limit is exceeded' do
-    balance_limit = Oystercard::BALANCE_LIMIT
-    subject.top_up(balance_limit)
-    expect{ subject.top_up(1) }.to raise_error(RuntimeError, "Top-up failed. Balance limit of #{balance_limit} exceeded.")
-  end
-
-  it 'deducts from the balance' do
-    subject.top_up(20)
-    subject.deduct(10)
     expect(subject.balance).to eq 10
   end
 
-  it '#touch_in returns true when touching in' do
-    expect(subject.touch_in).to eq true
+  it 'raises an error if balance limit is exceeded' do
+    expect{ subject.top_up(Oystercard::BALANCE_LIMIT + 1) }.to raise_error(RuntimeError, "Top-up failed. Balance limit of #{Oystercard::BALANCE_LIMIT} exceeded.")
   end
 
-  it '#touch_out returns false when touching out' do
-    expect(subject.touch_out).to eq false
+  describe 'top up for tests' do
+    before do
+      subject.top_up(Oystercard::MINIMUM_PERMITTED_FARE)
+      subject.touch_in
+    end
+
+    # it 'deducts from the balance' do
+    #   subject.deduct(10)
+    #   expect(subject.balance).to eq(-9)
+    # end
+
+    it '#touch_in returns true when touching in' do
+      expect(subject.touch_in).to eq true
+    end
+
+    it '#touch_out returns false when touching out' do
+      expect(subject.touch_out).to eq false
+    end
+
+    it '#in_journey lets us know if we are in journey when we have touched in' do
+      expect(subject.in_journey?).to eq true
+    end
+
+    it '#in_journey lets us know if we are in journey when we have touched out' do
+      subject.touch_out
+      expect(subject.in_journey?).to eq false
+    end
+
+    it "#touch_out deducts minimum fare once journey is complete" do
+      expect {subject.touch_out}.to change{subject.balance}.by(-1)
+    end
   end
 
-  it '#in_journey lets us know if we are in journey when we have touched in' do
-    subject.touch_in
-    expect(subject.in_journey?).to eq true
+  it "#touch_in throws an error when card doesn't have enough balance for a journey" do
+    expect{ subject.touch_in }.to raise_error(RuntimeError, "Not enough money on card")
   end
-  it '#in_journey lets us know if we are in journey when we have touched out' do
-    subject.touch_out
-    expect(subject.in_journey?).to eq false
-  end
+
 end
